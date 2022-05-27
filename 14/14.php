c683ae21e3d6e22e6610110be59addcdbb06ea3b
TranQuocHuya <86847384+TranQuocHuya@users.noreply.github.com>
2022-05-28 00:08:08 +0700

    add module 14

--- 14/14.php
+++ 14/14.php
@@ -9,7 +9,7 @@ if (!class_exists('lessc')) {
 }
 
 $less = new lessc;
-$less->compileFile('less/01.less', 'css/01.css');
+$less->compileFile('less/14.less', 'css/14.css');
 
 ?>
 
@@ -22,11 +22,11 @@ $less->compileFile('less/01.less', 'css/01.css');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>01</title>
-    <link href="./css/01.css" rel="stylesheet" type="text/css" />
+    <link href="./css/14.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-    <?php include $dir_block . '/01-content.php'; ?>
+    <?php include $dir_block . '/14-content.php'; ?>
 </body>
 
 </html>
