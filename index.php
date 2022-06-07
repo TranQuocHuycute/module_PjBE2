eb2999023ec0a8c9cfa0e62d2a9f6e96764f1d11
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-06-07 23:06:12 +0700

    thêm module chính

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>LaLiga live score, fixtures and results - SofaScore</title>
+	<!-- boostrap	 -->
+	<link rel="stylesheet" href="../demo/public/css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="../demo/public/css/index.css">
+	<link rel="stylesheet" href="../demo/public/css/style.css" type="text/css">
+	<link rel="stylesheet" href="../demo/public/css/style_title.css" type="text/css">
+
+
+
+
+</head>
+
+<body>
+	<ul class="sc-4461e154-0 address" style="margin-left: 185px;">
+		<li><a href="/">Football</a></li>
+		<li><a href="/football/spain">Spain</a></li>
+		<li>
+			<h1 class="sc-1b1c645e-0 gjYKiO">LaLiga live score</h1>
+		</li>
+	</ul>
+	<div class="container">
+		<div class="sc-fa55005a-0 jbMuLJ">
+
+		</div>
+		<div class="sc-e1b60555-0 hBrWcV">
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="sc-e1b60555-0 hBrWcV">
+
+						<!-- phần Standings and title -->
+						<?php include "./10/10.php" ?>
+						<?php include "./11/11.php" ?>
+						<!--end Standing  -->
+					</div>
+
+					<!-- Player Statistics -->
+					<?php include "./12/12.php" ?>
+					<!--end Player Statistics -->
+				</div>
+
+				<!-- Top Player -->
+				<?php include "./13/13.php" ?>
+				<!-- end Top Player -->
+			</div>
+		</div>
+	</div>
+	<script src="js/jquery-2.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
