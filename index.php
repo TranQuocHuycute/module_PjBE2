b41fffc768de6a810b3444c06cb11ee99f93f865
TranQuocHuya <86847384+TranQuocHuya@users.noreply.github.com>
2022-06-10 10:10:08 +0700

    up

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>LaLiga live score, fixtures and results - SofaScore</title>
 	<!-- boostrap	 -->
-	<link rel="stylesheet" href="../demo/public/css/bootstrap.min.css" type="text/css">
-	<link rel="stylesheet" href="../demo/public/css/index.css">
-	<link rel="stylesheet" href="../demo/public/css/style.css" type="text/css">
-	<link rel="stylesheet" href="../demo/public/css/style_title.css" type="text/css">
+	<link rel="stylesheet" href="./public/css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="./public/css/index.css">
+	<link rel="stylesheet" href="./public/css/style.css" type="text/css">
+	<link rel="stylesheet" href="./public/css/style_title.css" type="text/css">
 
 
 
