23306b944d6d41a9fc62ea1a450419d93996dd03
TranQuocHuya <86847384+TranQuocHuya@users.noreply.github.com>
2022-06-16 20:09:35 +0700

    up

---
+++ blocks/10/10.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+	<meta name="generator" content="Hugo 0.72.0">
+	<title>10</title>
+
+	
+
+</head>
+
+<body>
+<?php
+ include '10_content.php'; ?>
+</body>
+
+</html>	
---
+++ blocks/12/12.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.72.0">
+    <title>12</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style_title.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="./blocks/12/css/reset.css" type="text/css"> 
+
+</head>
+
+<body>
+<?php include "12_content.php"; ?>
+</body>
+
+</html>
